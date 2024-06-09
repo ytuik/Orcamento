3dbc5ba8cfd591aeea27434e8818baea02958faf
ytuik <ytuik99@gmail.com>
2024-06-09 19:45:09 -0300

    Change the database to Derby and remodeling the project infra

---
+++ orcamento-app/root/back-end/src/main/java/com/app/orcamento/models/Category/Category.java
@@ -0,0 +1,31 @@
+package com.app.orcamento.models.Category;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Category")
+public class Category {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
